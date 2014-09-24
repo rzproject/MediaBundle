bdef59439a87d7aa4e2aab9f408fa574026705a4
mellzamora <mellzamora@outlook.com>
2014-09-24 10:02:17 +0800

    removed category

--- Entity/Media.php
+++ Entity/Media.php
@@ -6,21 +6,5 @@ use Sonata\MediaBundle\Entity\BaseMedia;
 
 abstract class Media extends BaseMedia
 {
-     protected $category;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setCategory ($category)
-    {
-        $this->category = $category;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getCategory ()
-    {
-        return $this->category;
-    }
+    
 }
