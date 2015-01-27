65b077c98adee6bf7c741b85ed190fb63c95a957
mellzamora <mellzamora@outlook.com>
2015-01-27 17:27:42 +0800

    updated featured media settings

--- Block/FeatureMediaBlockService.php
+++ Block/FeatureMediaBlockService.php
@@ -35,6 +35,29 @@ class FeatureMediaBlockService extends BaseFeatureMediaBlockService
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function buildEditForm(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formatChoices = $this->getFormatChoices($block->getSetting('mediaId'));
+
+        $translator = $this->container->get('translator');
+
+        $formMapper->add('settings', 'sonata_type_immutable_array', array(
+            'keys' => array(
+                array('title', 'text', array('required' => false)),
+                array('content', 'ckeditor', array('config_name'=>'minimal_editor', 'attr'=>array('class'=>'span8'))),
+                array('orientation', 'choice', array('choices' => array(
+                    'left'  => $translator->trans('feature_left_choice', array(), 'SonataMediaBundle'),
+                    'right' => $translator->trans('feature_right_choice', array(), 'SonataMediaBundle')
+                ))),
+                array($this->getMediaBuilder($formMapper), null, array()),
+                array('format', 'choice', array('required' => count($formatChoices) > 0, 'choices' => $formatChoices)),
+            )
+        ));
+    }
+
+    /**
      * @param null|\Sonata\MediaBundle\Model\MediaInterface $media
      *
      * @return array
