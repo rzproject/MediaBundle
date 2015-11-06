6ecd58639f99b0e7e4fa93bfcb8204082109513d
mellzamora <mellzamora@outlook.com>
2015-11-06 15:21:14 +0800

    updated

--- Resources/public/js/block/rzmedia-featured-gallery.js
+++
@@ -1,9 +0,0 @@
-var rzmediafeaturedgallery = {
-    init:function(id){
-        console.log(id);
-        if(jQuery(id).length > 0){
-            console.log('rommel');
-            jQuery(id).imgLiquid();
-        }
-    }
-}
